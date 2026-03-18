fdc87daa8e6888d0803230be5db75018da5242ae
nirmalsingh16 <nirmal.singh@techsuperiors.com>
2026-03-18 17:35:04 +0530

    multi-step-form-code

---
+++ script/multiform.js
@@ -0,0 +1,95 @@
+let pageNo = 0;
+
+let progress = 0;
+
+// let data = {
+//   name: "",
+//   email: "",
+//   contact: "",
+//   dob: "",
+//   coursename: "",
+//   percentage: "",
+//   fathername: "",
+//   mothername: "",
+// };
+
+const pages = document.querySelectorAll(".Info");
+const nextbtn = document.querySelector(".nextbtn");
+const backbtn = document.querySelector(".backbtn");
+const ProgressBar = document.querySelector(".progress");
+
+// show first page
+showPage(pageNo);
+
+function showPage(index) {
+  pages.forEach((page, i) => {
+    page.style.display = i === index ? "block" : "none";
+  });
+
+  // disable back button on first page
+  backbtn.disabled = index === 0;
+
+  // disable next button on last page
+  nextbtn.disabled = index === pages.length;
+}
+
+function nextPage() {
+  if (pageNo <= pages.length) {
+    pageNo++;
+    showPage(pageNo);
+    calculateProgres();
+  }
+
+  ProgressBar.style.width = `${progress}%`;
+
+  nextbtn.innerHTML = pageNo >= pages.length - 1 ? "Submit" : "Next";
+  console.log(pageNo);
+
+  if (pageNo === pages.length) {
+    saveData();
+    nextbtn.innerHTML = "Submitted";
+    nextbtn.disabled = true;
+    nextbtn.style.display = "block";
+    nextbtn.style.cursor = "not-allowed";
+    backbtn.disabled = true;
+  }
+}
+
+function prevPage() {
+  if (pageNo > 0) {
+    pageNo--;
+    showPage(pageNo);
+    calculateProgres();
+    document.getElementById("resume").style.display = "none";
+  }
+  ProgressBar.style.width = `${progress}%`;
+}
+
+function calculateProgres() {
+  progress = (pageNo / pages.length) * 100;
+}
+
+function saveData() {
+  document.getElementById("rname").innerText =
+    document.getElementById("name").value;
+  document.getElementById("remail").innerText =
+    document.getElementById("email").value;
+  document.getElementById("rcontact").innerText =
+    document.getElementById("contact").value;
+  document.getElementById("rnumber").innerText =
+    document.getElementById("dob").value;
+  document.getElementById("rcoursename").innerText =
+    document.getElementById("coursename").value;
+  document.getElementById("rpercentage").innerText =
+    document.getElementById("percentage").value;
+  document.getElementById("rfathername").innerText =
+    document.getElementById("fathername").value;
+  document.getElementById("rmothername").innerText =
+    document.getElementById("mothername").value;
+
+  const resume = (document.getElementById("resume").style.display = "block");
+  console.log(resume);
+}
+
+nextbtn.addEventListener("click", nextPage);
+backbtn.addEventListener("click", prevPage);
